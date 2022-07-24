6dc9d096ec1791b92995506695af3717719a0e1d
Irini <763444647@qq.com>
2022-07-24 06:52:47 -0700

    完成sleep.c程序

---
+++ user/sleep.c
@@ -0,0 +1,26 @@
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int no,time,res;
+
+    if(argc<=1){
+        //错误提示参考Ubuntu系统sleep错误提示，并加上Usage
+        fprintf(2,"sleep: missing operand\nUsage: sleep time(int)");//为啥是2啊...输出到屏幕的意思吗...
+        exit(1);//为啥main要用exit退出？
+    }
+
+    //看Ubuntu中sleep的运行情况，当输入参数多于1个时，似乎会sleep各参数的累加时间
+    for(no=1;no<argc;no++){
+        //问题：使用atoi没办法判断"0"和"abcd"的错误输入...
+        if((res=atoi(argv[no]))==0){
+        fprintf(2,"sleep: invalid time interval %s",argv[no]);//为啥是2啊...输出到屏幕的意思吗...
+        exit(1);
+        }
+        time+=res;
+    }
+
+    //fprintf(2,"sleeping %d ticks",&time);
+    sleep(time);
+    exit(0);
+}
