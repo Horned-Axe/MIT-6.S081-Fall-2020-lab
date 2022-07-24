5b828ce4b5a6ed3d9702d1ff20cc849bce02f14e
Irini <763444647@qq.com>
2022-07-24 09:01:53 -0700

    修正pingpong.c中因逻辑语句错误引发的pipe不执行

--- user/pingpong.c
+++ user/pingpong.c
@@ -6,8 +6,7 @@ int main()
     //好有道理！可以有两个不同方向的管道啊
     int child_fd[2];//child导向father的管道
     int father_fd[2];//father导向child的管道
-    char buffer[128]={'\0'};
-    if((pipe(child_fd)==-1)&&(pipe(father_fd)==-1)){//创建管道//暂且当作-1时错误吧...
+    if((pipe(child_fd)==-1)||(pipe(father_fd)==-1)){//创建管道//暂且当作-1时错误吧...//！！！这里的逻辑错啦！语句放判断句里还是危险的...！
         fprintf(2,"pipe error!");
         exit(1);
     }
@@ -15,12 +14,13 @@ int main()
     int respid=fork();//创建子进程
     if(respid==0){
         //in child procedure
+        char buffer[16]={'\0'};
         int pid_t=getpid();
         close(father_fd[1]);
         close(child_fd[0]);
 
         read(father_fd[0],buffer,sizeof(buffer));
-        fprintf(2,"%d:received %s",pid_t,buffer);
+        fprintf(2,"%d:received %s\n",pid_t,buffer);
 
         char *words="pong";
         write(child_fd[1],words,strlen(words)+1);
@@ -28,6 +28,7 @@ int main()
     }
     else if(respid>0){
         //in father procedure
+        char buffer[16]={'\0'};
         int pid_t=getpid();
         close(father_fd[0]);
         close(child_fd[1]);
@@ -36,7 +37,7 @@ int main()
         write(father_fd[1], words, strlen(words) + 1); 
 
         read(child_fd[0],buffer,sizeof(buffer)); 
-        fprintf(2,"%d:received %s",pid_t,buffer);
+        fprintf(2,"%d:received %s\n",pid_t,buffer);
         exit(0);
     }
     else{
