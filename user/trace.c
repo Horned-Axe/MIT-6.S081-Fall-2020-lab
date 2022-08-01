4879ad655495fff220bde37efbd24a3e614afa62
Irini <763444647@qq.com>
2022-08-01 11:36:58 -0700

    前置部分：Makrfile，函数声明，trace的存根，syscall的编号etc.

--- user/trace.c
+++ user/trace.c
@@ -9,7 +9,7 @@ main(int argc, char *argv[])
   int i;
   char *nargv[MAXARG];
 
-  if(argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9')){
+  if(argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9')){//1.确保提供参数多于2个 2.确保第一个提供参数的首位是数字
     fprintf(2, "Usage: %s mask command\n", argv[0]);
     exit(1);
   }
@@ -19,7 +19,7 @@ main(int argc, char *argv[])
     exit(1);
   }
   
-  for(i = 2; i < argc && i < MAXARG; i++){
+  for(i = 2; i < argc && i < MAXARG; i++){//可参考的exec调用前置流程 //前面差不多也是这样的，不过没判断 i < MAXARG
     nargv[i-2] = argv[i];
   }
   exec(nargv[0], nargv);
